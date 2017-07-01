ad2719176cd90f66efa2b6d35152975cc0d7bd52
Sim Hui Min <huimin.sim.2015@smu.edu.sg>
2017-07-01 14:28:58 +0800

    created schema "iieportal" with tables user, mentee, company, task and program_stages

---
+++ iieportal.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 01, 2017 at 06:26 AM
+-- Server version: 5.7.11
+-- PHP Version: 5.6.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `iieportal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `company_id` int(11) NOT NULL,
+  `company_name` varchar(45) DEFAULT NULL,
+  `company_description` varchar(200) DEFAULT NULL,
+  `vision` varchar(100) DEFAULT NULL,
+  `mission` varchar(100) DEFAULT NULL,
+  `industry` varchar(45) DEFAULT NULL,
+  `current_stage` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mentee`
+--
+
+CREATE TABLE `mentee` (
+  `username` varchar(45) NOT NULL,
+  `user_type` varchar(45) DEFAULT NULL,
+  `company_id` varchar(45) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `program_stages`
+--
+
+CREATE TABLE `program_stages` (
+  `stage_no` int(11) NOT NULL,
+  `stage_name` varchar(125) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `program_stages`
+--
+
+INSERT INTO `program_stages` (`stage_no`, `stage_name`) VALUES
+(1, 'Business Model Validation Stage'),
+(2, 'Innovation Development Stage'),
+(4, 'Business Plan/ Financial Modelling Stage'),
+(3, 'Go-To-Market & Early Customer Acquisition Stage');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `task_id` int(11) NOT NULL,
+  `task_name` varchar(125) NOT NULL,
+  `task_description` varchar(200) NOT NULL,
+  `task_dateline` datetime DEFAULT NULL,
+  `program_stage` int(11) DEFAULT NULL,
+  `is_completed` varchar(45) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(45) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `nric` varchar(9) DEFAULT NULL,
+  `user_type` varchar(45) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`company_id`);
+
+--
+-- Indexes for table `mentee`
+--
+ALTER TABLE `mentee`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `program_stages`
+--
+ALTER TABLE `program_stages`
+  ADD PRIMARY KEY (`stage_no`);
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`task_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
