45ee92afbd3541cd53d4d89b210bd42ecb5e6c41
Sim Hui Min <huimin.sim.2015@smu.edu.sg>
2017-07-03 18:45:27 +0800

    committed the wrong sql file last time. so this is the correct one

--- iieportal.sql
+++ iieportal.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 02, 2017 at 03:51 PM
+-- Generation Time: Jul 03, 2017 at 10:41 AM
 -- Server version: 5.7.11
 -- PHP Version: 5.6.19
 
@@ -37,6 +37,69 @@ CREATE TABLE `company` (
   `current_stage` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mentee`
+--
+
+CREATE TABLE `mentee` (
+  `username` varchar(45) NOT NULL,
+  `user_type` varchar(45) DEFAULT NULL,
+  `company_id` varchar(45) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `program_stages`
+--
+
+CREATE TABLE `program_stages` (
+  `stage_no` int(11) NOT NULL,
+  `stage_name` varchar(125) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `program_stages`
+--
+
+INSERT INTO `program_stages` (`stage_no`, `stage_name`) VALUES
+(1, 'Business Model Validation Stage'),
+(2, 'Innovation Development Stage'),
+(4, 'Business Plan/ Financial Modelling Stage'),
+(3, 'Go-To-Market & Early Customer Acquisition Stage');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task`
+--
+
+CREATE TABLE `task` (
+  `task_id` int(11) NOT NULL,
+  `task_name` varchar(125) NOT NULL,
+  `task_description` varchar(200) NOT NULL,
+  `task_deadline` datetime DEFAULT NULL,
+  `program_stage` int(11) DEFAULT NULL,
+  `is_completed` char(2) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `username` varchar(45) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `nric` varchar(9) DEFAULT NULL,
+  `user_type` varchar(45) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -46,6 +109,30 @@ CREATE TABLE `company` (
 --
 ALTER TABLE `company`
   ADD PRIMARY KEY (`company_id`);
+
+--
+-- Indexes for table `mentee`
+--
+ALTER TABLE `mentee`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `program_stages`
+--
+ALTER TABLE `program_stages`
+  ADD PRIMARY KEY (`stage_no`);
+
+--
+-- Indexes for table `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`task_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`username`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
