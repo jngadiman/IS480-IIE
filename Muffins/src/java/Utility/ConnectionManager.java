64bf6e9b489ba676df702e30cf608f62e5f6e6ff
jngadiman.2014 <jngadiman.2014@sis.smu.edu.sg>
2017-08-16 20:31:18 +0800

    made changes to these files to deploy the project on aws

--- Muffins/src/java/Utility/ConnectionManager.java
+++ Muffins/src/java/Utility/ConnectionManager.java
@@ -54,7 +54,16 @@ public class ConnectionManager {
                 String port = props.getProperty("db.port");
                 String dbName = props.getProperty("db.name");
                 dbUser = props.getProperty("db.user");
-                dbPassword = props.getProperty("db.password");
+                
+                // grab environment variable to check if we are on production environment
+                String username = System.getProperty("os.name");
+                if (username.equals("Linux")) {
+                    // in production environment, use aws.db.password
+                    dbPassword = props.getProperty("aws.db.password");
+                } else {
+                    // in local environment, use db.password
+                    dbPassword = props.getProperty("db.password");
+                }
 
                 dbURL = "jdbc:mysql://" + host + ":" + port + "/" + dbName + "?useUnicode=true&characterEncoding=UTF-8";
             } catch (Exception ex) {
